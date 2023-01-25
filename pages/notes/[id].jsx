61e806af02c3b7b3814d2133016099e27f255e4d
Emmanuel Adu <emmanueladu@Emmanuels-MacBook-Pro.local>
2023-01-24 20:39:05 -0800

    Routing with Pages

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const BackLink = () => (
+  <Link href="/notes">
+    Notes
+  </Link>
+)
 
 export default () => {
-    const router = useRouter()
-    const { id }= router.query
-  
-    return (
+  const router = useRouter()
+  const { id }= router.query
+
+  return (
+    <div>
       <h1>Note: {id} </h1>
-    )
+      <BackLink />
+    </div>
+  )
 }
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,3 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-export default () => <h1>Note index path</h1>
+export default () => {
+  const router = useRouter()
+  const notes = Array.from({length: 15}, (_, i) => ({ id: i, title: `Note: ${i}` }))
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      <ul>
+        {notes.map(note => (
+          <li key={note.id}>
+            <Link href="/notes/[id]" as={`/notes/${note.id}`}>
+                <strong>{note.title}</strong>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={e => router.push('/')}>
+        Home
+      </button>
+    </div>
+  )
+}
